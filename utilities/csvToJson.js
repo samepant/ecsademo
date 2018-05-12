35a5f0abf04c3ab64d37b0c2907f16c719992d7e
samepant <sampanter@gmail.com>
2018-05-12 16:26:15 -0400

    adds script for csv to json conversion

---
+++ utilities/csvToJson.js
@@ -0,0 +1,37 @@
+const csv = require('csvtojson')
+const fs = require('fs')
+
+let csvFile
+
+if (process.argv[2]) {
+  csvFile = process.argv[2]
+} else {
+  console.log('I need a csv file as an argument :(')
+  process.exit(9)
+}
+
+let ecsaTeam = []
+let advisors = []
+csv()
+  .fromFile(csvFile)
+  .on('json', jsonObj => {
+    if (jsonObj.mainCategory === 'ECSA network') {
+      ecsaTeam.push(jsonObj)
+    } else if (jsonObj.mainCategory === 'Advisors') {
+      advisors.push(jsonObj)
+    }
+  })
+  .on('done', err => {
+    if (err) throw err;
+    const finalObject = {
+      'ecsaTeam': ecsaTeam,
+      'advisors': advisors
+    }
+    console.log(`I found ${ecsaTeam.length} ECSA team members and ${advisors.length} Advisors`)
+    const stringJson = JSON.stringify(finalObject)
+    fs.writeFile('../src/assets/team.json', stringJson, 'utf8', err => {
+      if (err) throw err;
+      console.log('and I saved a json file in \'../src/assets/team.json\'')
+      process.exit()
+    })
+  })
